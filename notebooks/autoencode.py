edda882254bec54ca1a08feabad0e7e6e4b6cc1b
Marie Roald <roald.marie@gmail.com>
2018-07-02 14:33:43 +0200

    Began cleaning up the code and adding some documentation

--- notebooks/autoencode.py
+++ notebooks/autoencode.py
@@ -2,6 +2,7 @@ from comet_ml import Experiment
 import keras.layers as kl
 import keras.models as km
 import keras.regularizers as kr
+import keras.optimizers as ko
 
 from keras import backend as K
 
@@ -24,7 +25,7 @@ import seaborn as sns
 
 class Autoencoder:
     def __init__(self, encoder_params, decoder_params, input_shape,
-                 latent_shape, optimizer_params, loss="mean_squared_error"):
+                 latent_shape, optimizer_params=None, loss="mean_squared_error"):
         """ """
         if encoder_params[-1]["kwargs"]["units"] != latent_shape[0]:
             raise ValueError("Latent shape must be equal to the number of units"
@@ -32,7 +33,7 @@ class Autoencoder:
 
         self._input_params = {k: v for k, v in locals().items() if k != 'self'}
 
-        encoder, decoder, ae = self.create_autoencoder(encoder_params,
+        encoder, decoder, ae = self._create_autoencoder(encoder_params,
                                                        decoder_params,
                                                        input_shape,
                                                        latent_shape)
@@ -40,14 +41,28 @@ class Autoencoder:
         self.decoder = decoder
         self.ae = ae
 
-        # TODO: fix that shit
-        self.ae.compile(optimizer="adam", loss=loss)
+        self.optimizer = self._create_optimizer(optimizer_params)
+        self.ae.compile(optimizer=self.optimizer, loss=loss)
 
-    def fit(self, X,*args, shuffle=True, epochs=500, batch_size=32, verbose=1,
-            **kwargs):
-        """ """
-        self.ae.fit(X, X, shuffle=shuffle, epochs=epochs, batch_size=batch_size,
-                    verbose=verbose, **kwargs)
+    def _create_optimizer(self, optimizer_params):
+        """Creates a keras optimizer from dict containing the parameters"""
+
+        if optimizer_params is None:
+            optimizer_params = {
+                "type": "SGD",
+                "kwargs": {
+                    "momentum": 0.9
+                }
+            }
+
+        optimizer_type = getattr(ko, optimizer_params["type"])
+        optimizer = optimizer_type(**optimizer_params["kwargs"])
+        return optimizer
+
+    def fit(self, X, *args, **kwargs):
+        """Trains a keras autoencoder model"""
+
+        self.ae.fit(X, X, **kwargs)
 
     def get_params(self, deep=False):
         return self._input_params
@@ -61,17 +76,17 @@ class Autoencoder:
     def decode(self, embedding):
         return self.decoder.predict(embedding)
 
-    def create_autoencoder(self, encoder_config, decoder_config,
+    def save(self, filename):
+        return self.ae.save(filename)
+
+    def _create_autoencoder(self, encoder_config, decoder_config,
                            input_shape=None, latent_shape=None):
         """Creates an autoencoder model from dicts containing the parameters"""
-        self.encoder_layers = self.create_layers(encoder_config,
+
+        self.encoder_layers = self._create_layers(encoder_config,
                                             input_shape=input_shape)
-        self.decoder_layers = self.create_layers(decoder_config,
+        self.decoder_layers = self._create_layers(decoder_config,
                                             input_shape=latent_shape)
-
-        #self.decoder_layers = self.create_layers(decoder_config)
-
-        input_layer = [kl.InputLayer(input_shape=latent_shape)]
 
         encoder = km.Sequential(self.encoder_layers)
         decoder = km.Sequential(self.decoder_layers)
@@ -79,7 +94,7 @@ class Autoencoder:
 
         return encoder, decoder, autoencoder_model
 
-    def create_layers(self, layer_dict_list, input_shape=None):
+    def _create_layers(self, layer_dict_list, input_shape=None):
         """Creates a list of keras layers from a list of parameter dicts"""
         layers = []
 
@@ -102,25 +117,83 @@ class Autoencoder:
 
         return layers
 
-def plot_loss(mean_loss, std_loss, steps):
-	colors = sns.color_palette()
-	fig = plt.figure()
-	ax = fig.add_subplot(111) 
-	ax.fill_between(steps, mean_loss + std_loss, np.maximum(mean_loss - std_loss, 0), facecolor=colors[1], alpha=0.2, color = colors[1], label='std')
-	ax.plot(steps, mean_loss, color = colors[1], linestyle="--")
-	return fig
+def plot_loss(mean_loss, std_loss, steps, color=None):
+    """Plot mean and standard deviation loss.
+    
+    
+    Parameters
+    ----------
+    mean_loss: np.array
+        Mean loss to plot as a dashed line
+    std_loss: np.array
+        Standard deviation of loss
+    steps: np.array
+        Steps corresponding to the loss
+        
+    """
+    if color is None:
+        color = sns.color_palette()[0]
+    fig = plt.figure()
+    ax = fig.add_subplot(111) 
+    ax.fill_between(steps, 
+                    mean_loss + std_loss, 
+                    np.maximum(mean_loss - std_loss, 0), 
+                    facecolor=color, 
+                    alpha=0.2, 
+                    color = color, 
+                    label='std')
+    ax.plot(steps, mean_loss, color=color, linestyle="--")
+    return fig
 
+def plot_mean_std_loss(loss, steps, color=None, legend="", ax=None):
+    """Plot mean and standard deviation of loss.
+    
+    Parameters
+    ----------
+    loss: ndarray
+        2D array containing the loss values (num runs x num steps)
+    steps: ndarray
+        1D array containing the steps corresponding to the loss
+    color: 
+    """
 
-def my_cross_validate(clf, data, groups, experiment, n_splits=10, standardize=True):
+    mean_loss = loss.mean(0)
+    std_loss = loss.std(0)
+
+    if color is None:
+        color = sns.color_palette()[0]
+    if ax is None:
+        fig = plt.figure()
+        ax = fig.add_subplot(111) 
+    else:
+        fig = ax.get_figure()
+
+    std_plot = ax.fill_between(steps, 
+                               mean_loss + std_loss, 
+                               np.maximum(mean_loss - std_loss, 0), 
+                               facecolor=color, 
+                               alpha=0.2, 
+                               color=color, 
+                               label=legend+' std')
+
+    ax.plot(steps, mean_loss, color=color, linestyle="--", label=legend+" mean")
+    ax.legend()
+    ax.set_xlabel("Step")
+    ax.set_ylabel("Loss")
+
+    return fig
+
+def my_cross_validate(clf, data, groups, experiment, n_splits=10, standardize=True, epochs=100):
     data = data.values
 
     group_kfold = GroupKFold(n_splits=n_splits)
 
     val_losses = []
+    train_losses = []
     val_errors = []
 
     for i, (train_index, test_index) in enumerate(group_kfold.split(data, data, groups)):
-        comet_logger = CrossEntropyCometLogger(experiment, f"cv_fold_{i}")
+        comet_logger = GroupedCometLogger(experiment, f"cv_fold_{i}")
         new_clf = clone(clf)
 
         train_data = data[train_index]
@@ -131,24 +204,31 @@ def my_cross_validate(clf, data, groups, experiment, n_splits=10, standardize=Tr
             test_data = scaler.transform(test_data)
 
         history = new_clf.fit(train_data,
+                              epochs=epochs,
                               validation_data=(test_data, test_data),
                               callbacks=[comet_logger])
 
         val_losses.append(comet_logger.val_loss)
+        train_losses.append(comet_logger.train_loss)
         
         predictions = new_clf.predict(test_data)
         val_rmse = np.sqrt(((predictions - test_data) ** 2).mean())
         val_errors.append(val_rmse)
 
+    fig = plt.figure()
+    ax = fig.add_subplot(111) 
+
     val_losses = np.array(val_losses)
-    mean_loss = val_losses.mean(0)
-    std_loss = val_losses.std(0)
-    fig = plot_loss(mean_loss, std_loss, np.array(comet_logger.val_steps))
+    train_losses = np.array(train_losses)
+    colors = sns.color_palette()
+    
+    plot_mean_std_loss(val_losses, np.array(comet_logger.val_steps), ax=ax, color=colors[0], legend="val")
+    plot_mean_std_loss(train_losses, np.array(comet_logger.train_steps), ax=ax, color=colors[1], legend="train")
     experiment.log_figure("fig_test", fig)
     fig.show()
     return val_errors
         
-class CrossEntropyCometLogger(keras.callbacks.Callback):
+class GroupedCometLogger(keras.callbacks.Callback):
 
     def __init__(self, experiment, log_name):
         self.experiment = experiment
@@ -162,7 +242,9 @@ class CrossEntropyCometLogger(keras.callbacks.Callback):
         
     def on_batch_end(self, batch, logs=None):
         logs = {} if logs is None else logs
-        self.experiment.log_metric(f"{self.log_name}_loss", logs.get('loss'), step=self.step_count)
+        self.experiment.log_metric(f"{self.log_name}_loss", 
+                                   logs.get('loss'), 
+                                   step=self.step_count)
         self.train_steps.append(self.step_count)
         self.train_loss.append(logs.get("loss"))
         self.step_count = self.step_count + 1 
@@ -170,11 +252,11 @@ class CrossEntropyCometLogger(keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs=None):
         logs = {} if logs is None else logs
         if "val_loss" in logs:
-            self.experiment.log_metric(f"{self.log_name}_val_loss", logs.get('loss'), step=self.step_count)
+            self.experiment.log_metric(f"{self.log_name}_val_loss", 
+                                       logs.get('loss'), 
+                                       step=self.step_count)
             self.val_steps.append(self.step_count)
             self.val_loss.append(logs.get("loss"))
-
-
 
 if __name__== "__main__":
     data1 = pd.read_csv("X1_train.csv", index_col=0)
@@ -244,7 +326,6 @@ if __name__== "__main__":
     clf = Pipeline(steps=[('standardscaler', scaler), ('autoencoder', ae)])
     
     experiment = Experiment(project_name="comet test", api_key="50kNmWUHJrWHz3FlgtpITIsB1")
-    experiment.log_metric("test2",np.array([2,4,6]), np.array([1,2,3]))
 
     """
     scores = cross_validate(clf, data, data, groups=groups,
@@ -254,5 +335,4 @@ if __name__== "__main__":
     """
     scores = my_cross_validate(ae, data, groups, experiment=experiment)
 
-    print([score/data.shape[1] for score in scores])
-
+    print(scores)
